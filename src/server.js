55c0fe276441bcc3d761de5db7ef4bc4276f7eaf
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-05-25 22:10:57 +0200

    .

---
+++ src/server.js
@@ -0,0 +1,33 @@
+import express from 'express';
+import pino from 'pino-http';
+import cors from 'cors';
+import { env } from './utilits/env';
+
+const PORT = Number(env('PORT', '3000'));
+
+export const setupServer = () => {
+  const app = express();
+
+  app.use(express.json());
+  app.use(cors());
+
+  app.use(
+    pino({
+      transport: {
+        target: 'pino-pretty',
+      },
+    }),
+  );
+
+  app.use('*', (req, res) => {
+    res.sendStatus(404).json({
+      message: 'Not found',
+    });
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+};
+
+setupServer();
