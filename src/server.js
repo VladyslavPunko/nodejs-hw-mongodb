4bd05b234ce1a7681414459e953e3f9b74e00c45
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-05-26 14:10:54 +0200

    by ID

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import pino from 'pino-http';
 import cors from 'cors';
 import { env } from './utilits/env.js';
-import { getAllContacts } from './services/contacts.js';
+import { getAllContacts, getContactById } from './services/contacts.js';
 
 const PORT = Number(env('PORT', '3000'));
 
@@ -22,6 +22,33 @@ export const setupServer = () => {
 
   app.get('/contacts', async (req, res) => {
     const contacts = await getAllContacts();
+    res.status(200).json({
+      message: 'Successfully found contacts!',
+      data: contacts,
+    });
+  });
+
+ app.get('/contacts/:contactId', async (req, res) => {
+        const contactId = req.params.contactId;
+        try {
+        const contact = await getContactById(contactId);
+        if (!contact) {
+            return res.status(404).json({
+                message: `There is no contact with id ${contactId}`,
+            });
+            };
+            res.status(200).json({
+            message: `Successfully found contact with id ${contactId}`,
+            data: contact,
+        });
+        } catch(error) {
+             return res.status(404).json({
+                message: `There is no contact with id ${contactId}`,
+            });
+        }
+    });
+
+    const contacts = await getContactById();
     res.status(200).json({
       message: 'Successfully found contacts!',
       data: contacts,
