e2336d4d531f3b1d3f25d5b3cd34a0583ddaec88
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-06-08 00:12:27 +0200

    filter

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -10,16 +10,20 @@ import createHttpError from 'http-errors';
 import { parsePaginationParams } from '../utilits/parsePaginationParams.js';
 
 import { parseSortParams } from '../utilits/parseSortParams.js';
+import { parseFilterParams } from '../utilits/parseFilterParams.js';
 
 export const getContactsController = async (req, res) => {
   const { page, perPage } = parsePaginationParams(req.query);
   const { sortBy, sortOrder } = parseSortParams(req.query);
+
+  const filter = parseFilterParams(req.query);
 
   const contacts = await getAllContacts({
     page,
     perPage,
     sortBy,
     sortOrder,
+    filter,
   });
   res.status(200).json({
     status: 200,
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -7,25 +7,31 @@ import { SORT_ORDER } from '../constants/index.js';
 export const getAllContacts = async ({
   page = 1,
   perPage = 10,
-  sortBy = '_id',
   sortOrder = SORT_ORDER.ASC,
+  sortBy = '_id',
+  filter = {},
 }) => {
   const limit = perPage;
   const skip = (page - 1) * perPage;
-
   const contactsQuery = ContactsCollection.find();
-  const contactsCount = await ContactsCollection.find()
-    .merge(contactsQuery)
-    .countDocuments();
 
-  const contacts = await contactsQuery
-    .skip(skip)
-    .limit(limit)
-    .sort({ [sortBy]: sortOrder })
-    .exec();
+  if (filter.isFavourite) {
+    contactsQuery.where('isFavourite').equals(filter.isFavourite);
+  }
+  if (filter.contactType) {
+    contactsQuery.where('contactType').equals(filter.contactType);
+  }
+
+  const [contactsCount, contacts] = await Promise.all([
+    ContactsCollection.find().merge(contactsQuery).countDocuments(),
+    contactsQuery
+      .skip(skip)
+      .limit(limit)
+      .sort({ [sortBy]: sortOrder })
+      .exec(),
+  ]);
 
   const paginationData = calculatePaginationData(contactsCount, perPage, page);
-
   return {
     data: contacts,
     ...paginationData,
