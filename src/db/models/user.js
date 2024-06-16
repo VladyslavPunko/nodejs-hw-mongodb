dbbfd941cf827f818bd2bd9e212d4c8fc7525fd1
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-06-16 21:02:44 +0200

    step 1 + 2

---
+++ src/db/models/user.js
@@ -0,0 +1,33 @@
+import { model, Schema } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+export const UsersCollection = model('Users', userSchema);
