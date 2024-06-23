35422214576ba24a8443cb0a72b750cec00e68c7
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-06-23 23:14:04 +0200

    create dir

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
+import { TEMPLATES_DIR, UPLOAD_DIR } from './constants/index.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 import { setupServer } from './server.js';
+import { createDirIfNotExists } from './utilits/createDirIfNotExists.js';
 
 const bootstrap = async () => {
   await initMongoConnection();
+  await createDirIfNotExists(TEMPLATES_DIR);
+  await createDirIfNotExists(UPLOAD_DIR);
   setupServer();
 };
 
