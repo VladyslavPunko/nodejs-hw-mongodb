ded44276947d9751806d89767ba7424609ecb594
VladyslavPunko <143653897+VladyslavPunko@users.noreply.github.com>
2024-06-24 16:53:26 +0200

    fix

--- src/routers/auth.js
+++ src/routers/auth.js
@@ -35,7 +35,7 @@ authRouter.post('/logout', ctrlWrapper(logoutUserController));
 authRouter.post('/refresh', ctrlWrapper(refreshUserController));
 
 authRouter.post(
-  '/request-reset-email',
+  '/send-reset-email',
   validateBody(requestResetEmailSchema),
   ctrlWrapper(requestResetEmailController),
 );
